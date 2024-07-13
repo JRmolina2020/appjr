9598a4ff0f6e3c803e12e51d4a52425b9dbcb50b
JRmolina2020 <31552404+JRmolina2020@users.noreply.github.com>
2024-07-12 22:09:12 -0500

    login

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="user" content="{{Auth::user()}}">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>appjr</title>
+  <title>appjr1</title>
 
 
   <!-- START JS PERMISSIONS -->
